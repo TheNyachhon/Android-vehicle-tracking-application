60432647f652751b3e3fd6d0fd2b7b2a14e95e36
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-04-14 17:43:33 +0530

    view vehicles done

--- view vehicles.php
+++ view vehicles.php
@@ -2,29 +2,32 @@
     <?php 
         include('index.php');
         include('supervisor page.php');
-        $result=$con->query("SELECT * FROM vehicles");
+        $Supervisor_ID=$_SESSION['Supervisor_ID'];
+        $result=$con->query("SELECT * FROM vehicles WHERE Supervisor_ID=$Supervisor_ID");
          if(mysqli_num_rows($result)>0){
              echo "<table class='center' border='1'>
              <tr>
-             <th><h1>Employee ID</th>
-             <th><h1>Name</h1></th>
-             <th><h1>Contact</h1></th>
-             <th><h1>Address</h1></th>
-             <th><h1>Designation</h1></th>
-             <th><h1>Salary</h1></th>
+             <th><h1>Vehicle Number</th>
+             <th><h1>Brand</h1></th>
+             <th><h1>Model</h1></th>
+             <th><h1>Status</h1></th>
              </tr>";
              while($row = mysqli_fetch_assoc($result))
              {       
                  echo "<tr>";       
-                 echo "<td>" . $row['Emp_ID'] . "</td>";          
-                 echo "<td>" . $row['Name'] . "</td>";         
-                 echo "<td>" . $row['Contact'] . "</td>";                
-                 echo "<td>" . $row['Address'] . "</td>"; 
-                 echo "<td>" . $row['Designation'] . "</td>"; 
-                 echo "<td>INR " . $row['Salary'] . "</td>"; 
+                 echo "<td>" . $row['Vehicle_No'] . "</td>";          
+                 echo "<td>" . $row['Brand'] . "</td>";         
+                 echo "<td>" . $row['Model'] . "</td>";     
+                 if($row['Status']=="Booked")     {
+                    echo "<td><p class='submitMsg'>" . $row['Status'] . "</p></td>";         
+                }else{
+                    echo "<td><p class='submitMsgD'>" . $row['Status'] . "</p></td>";  
+                }           
                  echo "</tr>";          
              }
              echo "</table>";
+         }else{
+            echo "<p class='submitMsg'><b>No Records Found!<b></p>";
          }
     ?>
     <br>
