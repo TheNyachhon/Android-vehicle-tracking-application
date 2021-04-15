ba1108065e96ceb239cb53ba28c5081c1a6408c8
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-04-15 22:51:30 +0530

    view driver fixed

--- view driver status.php
+++ view driver status.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html >
 <html>
     <?php 
         include('index.php');
@@ -9,12 +10,13 @@
         if(isset($_POST['Driver_ID']))
 	    {
             $Driver_ID = $_POST["Driver_ID"];  
-            $sql="SELECT * FROM vehicles_assigned WHERE Driver_ID='$Driver_ID'";
+            $sql="SELECT * FROM drivers WHERE Driver_ID='$Driver_ID' and Supervisor_ID='$Supervisor_ID'";
             $query = mysqli_query($con,$sql);
             $row = mysqli_fetch_assoc($query);
             if($row){
-                $sql2="SELECT * FROM drivers WHERE Driver_ID='$Driver_ID' and Supervisor_ID='$Supervisor_ID'";
+                $sql2="SELECT * FROM vehicles_assigned WHERE Driver_ID='$Driver_ID'";
                 $query2 = mysqli_query($con,$sql2);
+                $row2 = mysqli_fetch_assoc($query2);
                 echo "<br>";
                 if(mysqli_num_rows($query2)>0){
                     echo "<table class='center' border='1' style='float:left;margin: 30px;'>
@@ -23,28 +25,27 @@
                     <th><h1>Name</h1></th>
                     <th><h1>Contact</h1></th>
                     </tr>";
-                    $row2 = mysqli_fetch_assoc($query2);
+                    //$row = mysqli_fetch_assoc($query);
                     echo "<tr>";       
-                    echo "<td>" . $row2['Driver_ID'] . "</td>";          
-                    echo "<td>" . $row2['Name'] . "</td>";         
-                    echo "<td>" . $row2['Contact'] . "</td>";                  
+                    echo "<td>" . $row['Driver_ID'] . "</td>";          
+                    echo "<td>" . $row['Name'] . "</td>";         
+                    echo "<td>" . $row['Contact'] . "</td>";                  
                     echo "</tr>";
-                    echo "</table>";
-                    
+                    echo "</table>";   
+                    //Now the vehicle info
+                    echo "<table class='center' border='1' style='margin:30px;'>
+                    <tr>
+                    <th><h1>Vehicle Number</h1></th>
+                    </tr>";
+                    echo "<tr>";
+                    echo "<td>" .$row2['Vehicle_No'] . "</td>"; 
+                    echo "</table>";                 
                 }else{
-                    echo "<p class='submitMsg'><b>Searched Driver Not Found!<b></p>";
+                    echo "<p class='submitMsg'><b>Driver not assigned any vehicle<b></p>";
                 }
-                echo "<table class='center' border='1' style='margin:30px;'>
-                <tr>
-                <th><h1>Vehicle Number</h1></th>
-                </tr>";
-                echo "<tr>";
-                echo "<td>" .$row['Vehicle_No'] . "</td>"; 
-                echo "</table>";
-
             }
             else{
-                echo "<p class='submitMsg'><b>Driver not assigned any vehicle<b></p>";
+                echo "<p class='submitMsg'><b>Searched Driver Not Found!<b></p>";
             }
         }        
     ?>
@@ -58,4 +59,97 @@
             </form>
         </div>
     </body>
+    Google maps
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+    <body>
+    <div id="map"></div>
+
+    <script>
+      var customLabel = {
+        restaurant: {
+          label: 'R'
+        },
+        bar: {
+          label: 'B'
+        }
+      };
+
+        function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: new google.maps.LatLng(-33.863276, 151.207977),
+          zoom: 12
+        });
+        var infoWindow = new google.maps.InfoWindow;
+
+          // Change this depending on the name of your PHP or XML file
+          downloadUrl('https://storage.googleapis.com/mapsdevsite/json/mapmarkers2.xml', function(data) {
+            var xml = data.responseXML;
+            var markers = xml.documentElement.getElementsByTagName('marker');
+            Array.prototype.forEach.call(markers, function(markerElem) {
+              var id = markerElem.getAttribute('id');
+              var name = markerElem.getAttribute('name');
+              var address = markerElem.getAttribute('address');
+              var type = markerElem.getAttribute('type');
+              var point = new google.maps.LatLng(
+                  parseFloat(markerElem.getAttribute('lat')),
+                  parseFloat(markerElem.getAttribute('lng')));
+
+              var infowincontent = document.createElement('div');
+              var strong = document.createElement('strong');
+              strong.textContent = name
+              infowincontent.appendChild(strong);
+              infowincontent.appendChild(document.createElement('br'));
+
+              var text = document.createElement('text');
+              text.textContent = address
+              infowincontent.appendChild(text);
+              var icon = customLabel[type] || {};
+              var marker = new google.maps.Marker({
+                map: map,
+                position: point,
+                label: icon.label
+              });
+              marker.addListener('click', function() {
+                infoWindow.setContent(infowincontent);
+                infoWindow.open(map, marker);
+              });
+            });
+          });
+        }
+      function downloadUrl(url, callback) {
+        var request = window.ActiveXObject ?
+            new ActiveXObject('Microsoft.XMLHTTP') :
+            new XMLHttpRequest;
+
+        request.onreadystatechange = function() {
+          if (request.readyState == 4) {
+            request.onreadystatechange = doNothing;
+            callback(request, request.status);
+          }
+        };
+
+        request.open('GET', url, true);
+        request.send(null);
+      }
+
+      function doNothing() {}
+    </script>
+    <script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA4Irb5-DYgI-ec7P0Sq-EMMZStN45mLfE&callback=initMap">
+    </script>
+  </body>
 </html>
