fc99453368119fa00dd3996d38c8752ebfc7e38e
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-05-17 00:13:52 +0530

    get driver data function added, herokuapp

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "8567";
-    $dbname = "vehicle_tracking";       //database name
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "8567";
+    // $dbname = "vehicle_tracking";       //database name
+    // //making an connection
+    // $con = mysqli_connect($servername,$username,$password,$dbname);
+    // if(!$con){          //if $con does not return true
+    //     die("Connection failed due to ".mysqli_connect_error());
+    // }else{
+    //     //echo "Connection successful <br><br>";
+    // }
+    $servername = "sql6.freesqldatabase.com";
+    $username = "sql6412598";
+    $password = "p8xl7mppFZ";
+    $dbname = "sql6412598";       //database name
     //making an connection
     $con = mysqli_connect($servername,$username,$password,$dbname);
     if(!$con){          //if $con does not return true
         die("Connection failed due to ".mysqli_connect_error());
     }else{
-        //echo "Connection successful <br><br>";
+    //    echo "Connection successful <br><br>";
     }
 ?>
