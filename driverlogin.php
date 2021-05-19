83ea9f40b6ebd61a67d663b182d7ddbde7f90f81
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-05-19 21:12:19 +0530

    test

--- driverlogin.php
+++ driverlogin.php
@@ -12,7 +12,7 @@
         $row2 = mysqli_fetch_array($query);
         if($row2){   //if vehicle is assigned to driver
             $name=$row['Name'];
-            echo "Logged IN!!!";
+            echo "Login Successful.";
             echo "Welcome ".$name."!";
         }else{
             echo "Error! You have not been assigned a vehicle!";
