b880cba2ca97f7f46e23def91e801c07bea254e2
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-05-19 13:07:15 +0530

    driver login for app

--- driver login.php
+++ driver login.php
@@ -8,26 +8,11 @@
     $row = mysqli_fetch_array($query);
     if($row){   //if data exists
         $name=$row['Name'];
-        echo "Login Success!"
+        echo "Login Success!";
         echo "Welcome".$name;
         
     }else{  
         echo "Login Failed, Please Try again";
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>
-    HELLO
-    </h1>
-    
-</body>
-</html>
 
