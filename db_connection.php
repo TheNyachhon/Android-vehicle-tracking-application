4cc5a5888e291c1f4cfab79062e9e1ac2a09d521
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-05-20 22:23:47 +0530

    minor fix

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "icon" href ="img/Logo.svg">
-</head>
 <?php
     $servername = "sql6.freesqldatabase.com";
     $username = "sql6412598";
