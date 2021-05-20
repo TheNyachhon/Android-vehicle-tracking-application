f59a48040410155b48d6dacff602dcf4a1bd1ba1
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-05-20 12:06:11 +0530

    icon

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel = "icon" href ="img/Logo.svg">
+    <link rel = "icon" href ="img/logo.svg">
     <title>AV Tracking System</title>
 </head>
 <div class="nav-container">
