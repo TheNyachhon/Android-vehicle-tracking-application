a7cfe305a4acbb857c200e40c12c2b7d94d78d6f
Rujin Shrestha <63194470+TheNyachhon@users.noreply.github.com>
2021-05-20 12:04:21 +0530

    title fix

--- header.php
+++ header.php
@@ -1,6 +1,14 @@
 <?php
     include('db_connection.php');
 ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel = "icon" href ="img/Logo.svg">
+    <title>AV Tracking System</title>
+</head>
 <div class="nav-container">
     <div class="wrapper">
         <nav>
